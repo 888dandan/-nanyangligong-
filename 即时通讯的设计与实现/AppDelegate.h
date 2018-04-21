ca7b6a9076f682e161a45548fbef455c27adeb99
888dandan <1550713714@qq.com>
2018-04-21 08:40:35 +0800

    Initial Commit

---
+++ 即时通讯的设计与实现/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  即时通讯的设计与实现
+//
+//  Created by 曹轩 on 2018/4/21.
+//  Copyright © 2018年 曹轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
